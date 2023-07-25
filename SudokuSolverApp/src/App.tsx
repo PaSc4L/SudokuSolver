e3e85e11a0c05c7c1f6104ea10669b915941b653
PaSc4L <bcsaba04082@gmail.com>
2023-07-25 14:52:38 +0200

    formatting the home page
    Making a navbar and formatting the style of the page

--- SudokuSolverApp/src/App.tsx
+++ SudokuSolverApp/src/App.tsx
@@ -1,10 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
 import Welcome from "./Components/Welcome";
 
 function App() {
 
   return (
-    <Welcome></Welcome>
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Sudoku Solver</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Kezdőlap</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">Feliratozás</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">Sudoku megoldása</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+
+        </nav>
+        <Welcome></Welcome>
+      </>
   )
 }
 
--- SudokuSolverApp/src/Components/Welcome.tsx
+++ SudokuSolverApp/src/Components/Welcome.tsx
@@ -1,31 +1,18 @@
+import './Welcome.css';
 function Welcome(){
     return (
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Sajt</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
+            <>
+                <div className = "welcome-page-background">
+                    <hr className="border border-white border-2 opacity-100"/>
+                    <img src="../Images/sudoku-background.jpg"/>
+                    <h1>Olld meg a sudokut Sudoku Solverrel!</h1>
+                    <div className="welcome-paragraphs">
+                        <p>Próba szüveg ajfnjipsnfvjsdnvjiéasdnvjisdnvjild vjibsdvjiosdjiovb sdjiovb jisdb vjhiodvb sdbv jhi</p>
+                        <p>Próba szöveg csak egy kicsit több, mert sajtos makaróni és éertelmesebb pohár tele van vízzel és</p>
                     </div>
                 </div>
-            </nav>
+
+            </>
     );
 }
 export default Welcome;
